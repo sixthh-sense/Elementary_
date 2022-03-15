2a97a97af8babab60a76118c0aeb1c952249ac73
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-03-15 18:36:29 +0900

    Initial commit

---
+++ src/main/java/KYH/Elementary/Algorithm/SumCycle.java
@@ -0,0 +1,5 @@
+package KYH.Elementary.Algorithm;
+
+// # 1110
+public class SumCycle {
+}
---
+++ src/main/java/KYH/Elementary/ElementaryApplication.java
@@ -0,0 +1,13 @@
+package KYH.Elementary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementaryApplication.class, args);
+	}
+
+}
