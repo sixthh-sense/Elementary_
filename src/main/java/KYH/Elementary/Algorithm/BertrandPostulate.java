f11b0bd5a7569542a44c67192e764f2978edc96e
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-03-17 20:51:41 +0900

    AlphaCentauri - 법칙 찾기 좀더 공부하자

---
+++ src/main/java/KYH/Elementary/Algorithm/BertrandPostulate.java
@@ -0,0 +1,43 @@
+package KYH.Elementary.Algorithm;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/4948
+public class BertrandPostulate {
+
+    // 전체 맵(2n 최대값 246912)
+    public static boolean[] prime = new boolean[246913];
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        get_prime();	// 소수를 얻는 메소드 실행
+
+        while(true) {
+            int n = in.nextInt();
+            if(n == 0) break;	// n 이 0 일경우 종료
+
+            int count = 0;	// 소수 개수를 셀 변수
+
+            for(int i = n + 1; i <= 2 * n; i++) {
+                // false일 때 소수 -> count 증가
+                if(!prime[i]) count++;
+            }
+            System.out.println(count);
+        }
+    }
+
+    // 소수를 얻는 메소드
+    public static void get_prime() {
+        // 0 과 1 은 소수가 아니므로 참
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) { // 제곱근부터 따지기
+           if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) { // 무언가의 제곱이니까 소수 탈락 -> true -> 무언가의 배수인 것도 죄다 소수 탈락
+                prime[j] = true;
+            }
+        }
+    }
+}
--- src/main/java/KYH/Elementary/Algorithm/FlyMeToTheAlphaCentauri.java
+++ src/main/java/KYH/Elementary/Algorithm/FlyMeToTheAlphaCentauri.java
@@ -1,5 +1,34 @@
 package KYH.Elementary.Algorithm;
 
+import java.util.Scanner;
+
 // #1011: https://www.acmicpc.net/problem/1011
 public class FlyMeToTheAlphaCentauri {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int T = in.nextInt();	// 테스트 케이스
+
+        for(int i = 0; i < T; i++) {
+
+            int X = in.nextInt();
+            int Y = in.nextInt();
+
+            int distance = Y - X;	// 거리
+
+            int max = (int)Math.sqrt(distance);	// 정수형 정의 -> 자동 소수점 버림 (법칙은 직접 찾는 수밖에 없나?)
+
+            if(distance == max * max) {
+                System.out.println(max * 2 - 1);
+            }
+            else if(distance <= max * max + max) {
+                System.out.println(max * 2);
+            }
+            else {
+                System.out.println(max * 2 + 1);
+            }
+
+        }
+    }
 }
