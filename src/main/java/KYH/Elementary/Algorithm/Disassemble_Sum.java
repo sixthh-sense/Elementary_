ef9ce41652d6eb0b2ee249d2221c102606cd83d8
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-03-27 20:15:44 +0900

    3/27 두 문제 추가

--- src/main/java/KYH/Elementary/Algorithm/Disassemble_Sum.java
+++ src/main/java/KYH/Elementary/Algorithm/Disassemble_Sum.java
@@ -8,25 +8,25 @@ import java.io.InputStreamReader;
 public class Disassemble_Sum {
     public static void main(String[] args) throws IOException {
 
+        // Brute force
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int result = 0;
 
-        for(int i = 0; i < N; i++) {
-            int number = i; // 1부터 N까지
-            int sum = 0;	// 각 자릿수 합 변수
+        for(int i = 1; i <= N; i++) {
+            int number = i; // 그냥 N번 연산을 한다 수준
+            int sum = 0;	// 각 자릿수의 합
 
             while(number != 0) {
                 sum += number % 10;	// 각 자릿수 더하기(일의 자리수부터)
                 number /= 10; // 다음 자리수도 나머지로 구해야 하니 10으로 나누기
             }
-
             // 숫자 본인 + 각 자릿수 누적합이 같을 경우 (생성자)
             if(sum + i == N) {
-                result = i;
+                result = i; // i는 0부터 시작해서 점점 커짐. 제일 먼저 구한 값 = 최소값
                 break;
             }
-
+            // 생성자가 없는 경우는 어떻게?
         }
 
         System.out.println(result);
---
+++ src/main/java/KYH/Elementary/Algorithm/LostBrackets.java
@@ -0,0 +1,31 @@
+package KYH.Elementary.Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// - 뒤에 오는 수를 괄호 처리?
+//참고 사이트: https://jainn.tistory.com/346 [Dogfootruler Kim]
+// https://www.acmicpc.net/problem/1541
+public class LostBrackets {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] cal = br.readLine().split("-"); // 빼기를 기준으로 분리
+        int res = 0;
+
+        for (int i = 0; i < cal.length; i++) {
+            int sum = 0;
+            String[] cal2 = cal[i].split("\\+");
+            for (int j = 0; j < cal2.length; j++) {
+                sum += Integer.parseInt(cal2[j]);
+            }
+            if (i == 0) {
+                res += sum;
+            } else {
+                res -= sum;
+            }
+        }
+        System.out.println(res);
+    }
+}
