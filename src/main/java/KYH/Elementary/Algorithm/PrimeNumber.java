d3108e85bf7a4edc1b11ab50291441f58a1c5396
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-04-23 18:39:09 +0900

    4/23

--- src/main/java/KYH/Elementary/Algorithm/PrimeNumber.java
+++ src/main/java/KYH/Elementary/Algorithm/PrimeNumber.java
@@ -1,15 +1,20 @@
 package KYH.Elementary.Algorithm;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
+//import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/1929
 public class PrimeNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        //Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int M, N;
-        M = sc.nextInt();
-        N = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
         for (int i = M; i <= N; i++) {
             if (isItPrime(i)) {
@@ -22,7 +27,7 @@ public class PrimeNumber {
         if (number < 2) {
             return false;
         } else {
-            for (int i = 2; i <= Math.sqrt(number); i++) {
+            for (int i = 2; i <= Math.sqrt(number); i++) { // 에라스토테네스의 체
                 if (number % i == 0)
                     return false;
             }
