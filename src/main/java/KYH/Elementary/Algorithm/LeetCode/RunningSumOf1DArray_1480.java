1c98d3f0ca5b75947d4db1cc08f14ce9b6a05dac
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-10-03 17:31:14 +0900

    LeetCode - Beginner #1480 몸풀기용 문제

---
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/RunningSumOf1DArray_1480.java
@@ -0,0 +1,21 @@
+package KYH.Elementary.Algorithm.LeetCode;
+
+import java.util.Arrays;
+
+public class RunningSumOf1DArray_1480 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int[] answer = new int[nums.length];
+        int cal = 0;
+        for (int i = 0; i < nums.length; i++) {
+            cal += nums[i];
+            answer[i] = cal;
+        }
+        return answer;
+    }
+}
