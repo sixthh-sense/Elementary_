b81cd874da12f22a0b47035040a70d2e30c836e0
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-10-06 23:31:07 +0900

    LeetCode - Beginner #1672 몸풀기용 문제 #2

---
+++ src/main/java/KYH/Elementary/javaAlone/RunStatementExample.java
@@ -0,0 +1,12 @@
+package KYH.Elementary.javaAlone;
+
+// source file(.java) 작성 -> javac 명령어 compile(.class byte code file 생성) -> java 명령어 실행 -> 실행결과 확인
+// 주석 - byte code file 크기까지 증가 X
+public class RunStatementExample {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int result = x + y;
+        System.out.println(result);
+    }
+}
