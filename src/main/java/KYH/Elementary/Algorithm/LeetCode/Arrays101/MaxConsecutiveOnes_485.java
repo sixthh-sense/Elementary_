e7c75b80eee287756183f81ef8974fd1d60e70e4
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-10-10 00:00:18 +0900

    leetcode 485 max consecutive ones

---
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/Arrays101/MaxConsecutiveOnes_485.java
@@ -0,0 +1,38 @@
+package KYH.Elementary.Algorithm.LeetCode.Arrays101;
+
+public class MaxConsecutiveOnes_485 {
+    // return the maximum number of consecutive 1s in the array
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxRecord = 0;
+        int count = 0;
+
+        /* for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count += 1;
+            }
+            if (nums[i] != 1 || i == (nums.length - 1)) {
+                if (count > maxRecord) {
+                    maxRecord = count;
+                }
+                count = 0;
+            }
+        } */
+        // 출처: https://leetcode.com/problems/max-consecutive-ones/discuss/96693/Java-4-lines-concise-solution-with-explanation
+        /* for (int num : nums) {
+            maxRecord = Math.max(maxRecord, count = (num == 0 ? 0 : count + 1));
+        }
+        return maxRecord; */
+        for (int i = 0; i < nums.length; i++) { // enhanced for문을 사용했는데 runtime 증가?
+            if (nums[i] == 1) {
+                count++;
+                maxRecord = Math.max(count, maxRecord);
+            }
+            else count = 0;
+        }
+        return maxRecord;
+    }
+}
