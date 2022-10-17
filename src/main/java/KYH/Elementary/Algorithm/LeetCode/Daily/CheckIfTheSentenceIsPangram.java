74ea3bccc5ce6936b3149112163245d7ebfef446
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-10-17 22:12:53 +0900

    leetcode 1832 Check if the Sentence Is Pangram - 주석(다른 풀이) 추가

--- src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/CheckIfTheSentenceIsPangram.java
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/CheckIfTheSentenceIsPangram.java
@@ -7,6 +7,7 @@ public class CheckIfTheSentenceIsPangram {
     }
     // 출처: https://leetcode.com/problems/check-if-the-sentence-is-pangram/discuss/1201598/Simple-java-0ms
     // 다른 방법으로는 Set<Character> set = new HashSet<>();, sentence.charAt(i) 등을 이용하는 것도
+    // char에서 'a'를 빼서 boolean[26] int[26] 같은 데 넣고 풀 수도
     public static boolean checkIfPangram(String sentence) {
         if (sentence.length() < 26) return false;
         for (char i = 'a'; i <= 'z'; i++) { // int뿐만이 아니라 char도 증가 가능
