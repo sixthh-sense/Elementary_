9ebf8fbafc81195b8504ce2ca92a8a9446a4fa6c
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-01-02 11:34:28 +0900

    0ms 링크 추가

--- src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/WordPattern_290.java
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/WordPattern_290.java
@@ -11,13 +11,16 @@ public class WordPattern_290 {
     * and a non-empty word in s
     * */
 
+    // LeetCode 0ms submission: https://leetcode.com/problems/word-pattern/submissions/869171719/
+
     public static String pattern = "abba"; // "aaaa"
     public static String s = "dog cat cat dog"; // "dog cat cat fish"
 
     public static boolean wordPattern(String pattern, String s) {
+        String[] array = s.split(" ");
+
         Map<Character, String> charMap = new HashMap<>();
         Map<String, Character> strMap = new HashMap<>();
-        String[] array = s.split(" ");
 
         if (pattern.length() != array.length) {
             return false;
@@ -43,6 +46,9 @@ public class WordPattern_290 {
     }
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         System.out.println(wordPattern(pattern, s));
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.println(estimatedTime); // 이것저것 덧붙이면 estimate이 너무 많이 달라짐? - 편차가 있으니 역시 JMeter로 검사하는 게 더 낫나
     }
 }
