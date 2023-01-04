e35802a8d8fcba27378d951e63a556e4c971582d
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-01-04 23:29:35 +0900

    리트코드_최소runtime 풀이(9ms) 주석으로 첨부

--- src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/MinimumRoundsToCompleteAllTasks_2244.java
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/MinimumRoundsToCompleteAllTasks_2244.java
@@ -39,3 +39,24 @@ public class MinimumRoundsToCompleteAllTasks_2244 {
         return answer;
     }
 }
+
+/* runtime 9ms leetcode 풀이
+    public int minimumRounds(int[] tasks) {
+        Arrays.sort(tasks);
+        int res = 0;
+        for (int i = 0; i < tasks.length;) {
+            int j = i + 1;
+            while (j < tasks.length && tasks[j] == tasks[i]) j++;
+            if (j == i + 1) return -1;
+            res += compute(j - i);
+            i = j;
+        }
+        return res;
+    }
+
+    private int compute(int i) {
+        if (i % 3 == 0) return i / 3;
+        if (i % 3 == 2) return (i - 2) / 3 + 1;
+        return (i - 4) / 3 + 2;
+    }
+* */
