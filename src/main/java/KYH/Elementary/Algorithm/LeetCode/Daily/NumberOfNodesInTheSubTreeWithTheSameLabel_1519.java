9dfeeceb26b4ae68804f6e01f11e53d1ea42b157
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-01-12 22:48:41 +0900

    1/12 바빠서 링크만 올려놓음

---
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/NumberOfNodesInTheSubTreeWithTheSameLabel_1519.java
@@ -0,0 +1,11 @@
+package KYH.Elementary.Algorithm.LeetCode.Daily;
+
+public class NumberOfNodesInTheSubTreeWithTheSameLabel_1519 {
+    /*
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        return null;
+    }
+     */
+    // official solution link: I'm just uploading the link now but I should swirl it into my brain later time
+    // https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/solutions/2864718/number-of-nodes-in-the-sub-tree-with-the-same-label/?orderBy=most_votes
+}
---
+++ src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/NumberOfNodesInTheSubTreeWithTheSameLabel1519Test.java
@@ -0,0 +1,10 @@
+package KYH.Elementary.Algorithm.LeetCode.Daily;
+
+import org.junit.jupiter.api.Test;
+
+class NumberOfNodesInTheSubTreeWithTheSameLabel1519Test {
+
+    @Test
+    void countSubTrees() {
+    }
+}
