b93c90d8fcc1222140d2d45f17023409c4682330
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-01-15 18:29:15 +0900

    1/15 이전 커밋 실수: 1/16이 아니라 1/15 Number of Good Paths: Hard, not solved by myself(2023/01/15) - computeIfAbsent와 좀더 친밀해지기 - 공식 풀이 이해 중

---
+++ src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/NumberOfGoodPaths2421Test.java
@@ -0,0 +1,23 @@
+package KYH.Elementary.Algorithm.LeetCode.Daily;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberOfGoodPaths2421Test {
+
+    @Test
+    void numberOfGoodPaths() {
+        assertAll(
+                () -> test(new int[] {1, 3, 2, 1, 3}, new int[][] {{0, 1}, {0, 2}, {2, 3}, {2, 4}}, 6),
+                () -> test(new int[] {1, 1, 2, 2, 3}, new int[][] {{0, 1}, {1, 2}, {2, 3}, {2, 4}}, 7),
+                () -> test(new int[] {1}, new int[][] {{}}, 1)
+        );
+    }
+
+    private void test(int[] vals, int[][] edges, int expected) {
+        NumberOfGoodPaths_2421 number = new NumberOfGoodPaths_2421();
+        int actual = number.numberOfGoodPaths(vals, edges);
+        assertEquals(expected, actual);
+    }
+}
