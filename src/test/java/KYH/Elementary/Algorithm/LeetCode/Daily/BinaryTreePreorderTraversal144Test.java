700c785e34f494ecbe95f24215e4a7c8d84367b9
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-01-15 13:33:06 +0900

    1/16 테스트 코드 안 풀린 원인 찾아서 수정 TreeNode 표기 방식이 각 문제마다 달라서 생겼던 문제

--- src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/BinaryTreePreorderTraversal144Test.java
+++ src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/BinaryTreePreorderTraversal144Test.java
@@ -7,26 +7,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BinaryTreePreorderTraversal144Test {
 
-    /* currently wrong: have to redo
+
     @Test
-    void preorderTraversal() {
-        assertAll(
-                () -> test(new Integer[] {1, null, 2, 3}, new ArrayList<Integer>(Arrays.asList(1, 2, 3))),
-                () -> test(new Integer[] {}, new ArrayList<Integer>()),
-                () -> test(new Integer[] {1}, new ArrayList<Integer>(Arrays.asList(1)))
-        );
+    void first() {
+        // 938번, 2181번 기준 TreeNode라서 144번 TreeNode 표기법과 다른 부분 있음 - 144번 리트코드에선 {1, null, 2, 3}가 입력값.
+        TreeNode treeNode = TreeNode.makeTree(new Integer[] {1, null, 2, null, null, 3});
+        //TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
+        List<Integer> actual = BinaryTreePreorderTraversal_144.preorderTraversal(treeNode);
+        assertThat(new ArrayList<>(Arrays.asList(1, 2, 3))).usingRecursiveComparison().isEqualTo(actual);
     }
 
-    private void test(Integer[] root, ArrayList<Integer> expected) {
-        TreeNode treeNode = TreeNode.makeTree(root);
-        BinaryTreePreorderTraversal_144 binaryTreePreorderTraversal_144 = new BinaryTreePreorderTraversal_144();
-        List<Integer> actual = binaryTreePreorderTraversal_144.preorderTraversal(treeNode);
-        assertEquals(expected, actual);
+    @Test
+    void second() {
+        // 938번, 2181번 기준 TreeNode라서 144번 TreeNode 표기법과 다른 부분 있음 - 144번 리트코드에선 {1, null, 2, 3}가 입력값.
+        TreeNode treeNode = TreeNode.makeTree(new Integer[] {});
+        //TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
+        List<Integer> actual = BinaryTreePreorderTraversal_144.preorderTraversal(treeNode);
+        assertThat(new ArrayList<>()).usingRecursiveComparison().isEqualTo(actual);
     }
-    */
+
+    @Test
+    void third() {
+        // 938번, 2181번 기준 TreeNode라서 144번 TreeNode 표기법과 다른 부분 있음 - 144번 리트코드에선 {1, null, 2, 3}가 입력값.
+        TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
+        //TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
+        List<Integer> actual = BinaryTreePreorderTraversal_144.preorderTraversal(treeNode);
+        assertThat(new ArrayList<>(List.of(1))).usingRecursiveComparison().isEqualTo(actual);
+    }
 }
--- src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/MinimumTimeToCollectAllApplesInATree1443Test.java
+++ src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/MinimumTimeToCollectAllApplesInATree1443Test.java
@@ -13,15 +13,15 @@ class MinimumTimeToCollectAllApplesInATree1443Test {
     @Test
     void minTime() {
         assertAll(
-                () -> test(7, new int[][]{{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6}}, new ArrayList<Boolean>(Arrays.asList(false, false, true, false, true, true, false)), 8),
-                () -> test(7, new int[][]{{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6}}, new ArrayList<Boolean>(Arrays.asList(false, false, true, false, false, true, false)), 6),
-                () -> test(7, new int[][]{{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6}}, new ArrayList<Boolean>(Arrays.asList(false, false, false, false, false, false, false)), 0)
+                () -> test(7, new int[][]{{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6}}, new ArrayList<>(Arrays.asList(false, false, true, false, true, true, false)), 8),
+                () -> test(7, new int[][]{{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6}}, new ArrayList<>(Arrays.asList(false, false, true, false, false, true, false)), 6),
+                () -> test(7, new int[][]{{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6}}, new ArrayList<>(Arrays.asList(false, false, false, false, false, false, false)), 0)
         );
     }
 
     private void test(int n, int[][] edges, List<Boolean> hasApple, int expected) {
         MinimumTimeToCollectAllApplesInATree_1443 minimumTimeToCollectAllApplesInATree_1443 = new MinimumTimeToCollectAllApplesInATree_1443();
         int actual = minimumTimeToCollectAllApplesInATree_1443.minTime(n, edges, hasApple);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
