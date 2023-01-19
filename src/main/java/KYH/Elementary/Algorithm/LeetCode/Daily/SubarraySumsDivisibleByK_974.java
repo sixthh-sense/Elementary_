46b68c6dddae49dde9eedfcf70ae4fc637808d6c
haein.sung <blue6zombie6@gmail.com>
2023-01-19 14:40:40 +0900

    공식 해설 봄 -> 개념 이해가 덜 됨 영어로 된 연산명을 잘 이해하지 못하는 게 이해 저해 요소

--- src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/SubarraySumsDivisibleByK_974.java
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/SubarraySumsDivisibleByK_974.java
@@ -3,7 +3,43 @@ package KYH.Elementary.Algorithm.LeetCode.Daily;
 import java.util.Arrays;
 
 public class SubarraySumsDivisibleByK_974 {
-    public int subarraysDivByK(int[] nums, int k) {
+    //prefix sum: 구간 합(앞쪽에서 더하는 건 prefix 뒤쪽은 suffix)
+    // modulo: 나머지 연산
+    // dividend: 피제수 (나눌 제)
+    // divisor: 제수
+    // quotinent: 몫
+    // remainder: 나머지
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,0,-2,-3,1};
+        int k = 5;
+        System.out.println(subarraysDivByK(nums, k));
+    }
+    public static int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length;
+        int prefixMod = 0, result = 0;
+
+        // There are k mod groups 0...k-1.
+        int[] modGroups = new int[k];
+        modGroups[0] = 1;
+
+        for (int num: nums) {
+            // Take modulo twice to avoid negative remainders.
+            prefixMod = (prefixMod + num % k + k) % k;
+            System.out.println("prefixMod: " + prefixMod);
+            // Add the count of subarrays that have the same remainder as the current
+            // one to cancel out the remainders.
+            result += modGroups[prefixMod]; // 이전에 기록된 나눗셈 값과 "같을" 때(즉 나눠떨어짐) 유의미한 값(0이 아닌)이 더해진다
+            System.out.println("result: " + result);
+            modGroups[prefixMod]++;
+            System.out.println("modGroups_update: " + Arrays.toString(modGroups));
+        }
+
+        System.out.println("modGroups_complete: " + Arrays.toString(modGroups));
+
+        return result;
+
+        /* Time limit exceeded
         int length = nums.length;
         int answer = 0, start = 0, arraySum = 0;
         while (start < length) {
@@ -16,6 +52,6 @@ public class SubarraySumsDivisibleByK_974 {
             arraySum = 0;
             start += 1;
         }
-        return answer;
+        return answer; */
     }
 }
