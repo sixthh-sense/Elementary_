7ed83f2c1471820ad39f97b588de4dcd50003635
haein.sung <blue6zombie6@gmail.com>
2023-01-20 11:12:06 +0900

    daily 문제를 풀 때는 80%의 경우 해석 보기에 급급한데 Explore 등으로 차근차근 문제 난이도를 올리고 싶다. 해설을 보면 왜 이렇게 짰는지 알겠는데 백지 상태에선 못 써내는 상태가 계속. 3일 후에 복습 각.

--- src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/NonDecreasingSubsequences_491.java
+++ src/main/java/KYH/Elementary/Algorithm/LeetCode/Daily/NonDecreasingSubsequences_491.java
@@ -1,9 +1,40 @@
 package KYH.Elementary.Algorithm.LeetCode.Daily;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NonDecreasingSubsequences_491 {
-    public List<List<Integer>> findSubsequences(int[] nums) {
-        return null;
+    // 시작점과 종점 nC2 = n * (n - 1) / 2
+    // Arrays.sort()를 한 결과물과 맞지 않으면 빼는 걸로 해도 될 것 같긴 한데 Time limit exceeded 될 것 같다
+    // 다른 subsequence 문제와 헷갈릴 뻔했는데 여기서는 기존 숫자배열에서 고를 때 "연속되지 않아도 괜찮음"
+    // call recursively 이 표현 주의
+
+    private static void backtrack(int[] nums, int index, List<Integer> sequence, Set<List<Integer>> answer) {
+        int length = nums.length;
+        if (index == length) {
+            if (sequence.size() >= 2) {
+                answer.add(new ArrayList<>(sequence));
+            }
+            return;
+        }
+        if (sequence.isEmpty() || sequence.get(sequence.size() -1) <= nums[index]) {
+            sequence.add(nums[index]);
+            backtrack(nums, index + 1, sequence, answer);
+            sequence.remove(sequence.get(sequence.size() - 1));
+        }
+        backtrack(nums, index + 1, sequence, answer);
+    }
+    public static List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> answer = new HashSet<>();
+        List<Integer> sequence = new ArrayList<>();
+        backtrack(nums, 0, sequence, answer);
+        return new ArrayList<>(answer);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 6, 7, 7};
+        System.out.println(findSubsequences(nums));
     }
 }
--- src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/NonDecreasingSubsequences491Test.java
+++ src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/NonDecreasingSubsequences491Test.java
@@ -7,17 +7,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class NonDecreasingSubsequences491Test {
-
-    NonDecreasingSubsequences_491 nonDecreasingSubQ = new NonDecreasingSubsequences_491();
 
     @Test
     void case1() {
         int[] nums = {4, 6, 7, 7};
         List<List<Integer>> expected = new ArrayList<>(Arrays.asList(Arrays.asList(4, 6), Arrays.asList(4, 6, 7), Arrays.asList(4, 6, 7, 7), Arrays.asList(4, 7), Arrays.asList(4, 7, 7), Arrays.asList(6, 7), Arrays.asList(6, 7, 7), Arrays.asList(7, 7))) ;
-        List<List<Integer>> actual = nonDecreasingSubQ.findSubsequences(nums);
+        List<List<Integer>> actual = NonDecreasingSubsequences_491.findSubsequences(nums);
         assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 
@@ -25,7 +22,7 @@ class NonDecreasingSubsequences491Test {
     void case2() {
         int[] nums = {4, 4, 3, 2, 1};
         List<List<Integer>> expected = new ArrayList<>(List.of(Arrays.asList(4, 4))) ;
-        List<List<Integer>> actual = nonDecreasingSubQ.findSubsequences(nums);
+        List<List<Integer>> actual = NonDecreasingSubsequences_491.findSubsequences(nums);
         assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 }
