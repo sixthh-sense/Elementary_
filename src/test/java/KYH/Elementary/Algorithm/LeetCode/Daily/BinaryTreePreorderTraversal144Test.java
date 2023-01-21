a78e03bce6917ac52fdcddc27384f9fe79034f14
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-01-21 14:20:03 +0900

    위치 등 살짝 수정(별 거 없음)

--- src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/BinaryTreePreorderTraversal144Test.java
+++ src/test/java/KYH/Elementary/Algorithm/LeetCode/Daily/BinaryTreePreorderTraversal144Test.java
@@ -15,7 +15,7 @@ class BinaryTreePreorderTraversal144Test {
     @Test
     void first() {
         // 938번, 2181번 기준 TreeNode라서 144번 TreeNode 표기법과 다른 부분 있음 - 144번 리트코드에선 {1, null, 2, 3}가 입력값.
-        TreeNode treeNode = TreeNode.makeTree(new Integer[] {1, null, 2, null, null, 3});
+        TreeNode treeNode = TreeNode.makeTree(new Integer[]{1, null, 2, null, null, 3});
         //TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
         List<Integer> actual = BinaryTreePreorderTraversal_144.preorderTraversal(treeNode);
         assertThat(new ArrayList<>(Arrays.asList(1, 2, 3))).usingRecursiveComparison().isEqualTo(actual);
@@ -24,7 +24,7 @@ class BinaryTreePreorderTraversal144Test {
     @Test
     void second() {
         // 938번, 2181번 기준 TreeNode라서 144번 TreeNode 표기법과 다른 부분 있음 - 144번 리트코드에선 {1, null, 2, 3}가 입력값.
-        TreeNode treeNode = TreeNode.makeTree(new Integer[] {});
+        TreeNode treeNode = TreeNode.makeTree(new Integer[]{});
         //TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
         List<Integer> actual = BinaryTreePreorderTraversal_144.preorderTraversal(treeNode);
         assertThat(new ArrayList<>()).usingRecursiveComparison().isEqualTo(actual);
@@ -33,7 +33,7 @@ class BinaryTreePreorderTraversal144Test {
     @Test
     void third() {
         // 938번, 2181번 기준 TreeNode라서 144번 TreeNode 표기법과 다른 부분 있음 - 144번 리트코드에선 {1, null, 2, 3}가 입력값.
-        TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
+        TreeNode treeNode = TreeNode.makeTree(new Integer[]{1});
         //TreeNode treeNode = TreeNode.makeTree(new Integer[] {1});
         List<Integer> actual = BinaryTreePreorderTraversal_144.preorderTraversal(treeNode);
         assertThat(new ArrayList<>(List.of(1))).usingRecursiveComparison().isEqualTo(actual);
